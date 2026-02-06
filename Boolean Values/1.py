2546e1aaa1ec59a31a3cdafe166d8cecf7fab2ae
adik-362 <salauatadilet.77@gmail.com>
2026-02-06 19:44:18 +0600

    prc2

--- Boolean Values/1.py
+++ Boolean Values/1.py
@@ -1,6 +1,5 @@
 a = 200
 b = 33
-
 if b > a:
   print("b is greater than a")
 else:
---
+++ Switch/If Elif/Else/4.py
@@ -0,0 +1,5 @@
+age = 16
+if age < 18:
+  pass # TODO: Add underage logic later
+else:
+  print("Access granted")
